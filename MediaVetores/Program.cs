b463fcabd2ad5ff17aecc1ad882b3c9a77bf8f2b
Marcos Anjos <marcosbanjos@gmail.com>
2023-11-16 19:21:35 -0300

    Praticando o uso de vetores

---
+++ MediaVetores/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MediaVetores
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+              Crie um programa que peça o nome e 4 notas de 10 alunos. 
+              Ao final o programa deve mostrar a média de cada um dos alunos.
+            */
+
+            string[] nome = new string[2];
+            decimal[] media = new decimal[2];
+            decimal nota1 = 0;
+            decimal nota2 = 0;
+            decimal nota3 = 0;
+            decimal nota4 = 0;
+
+            for (int i = 0; i < nome.Length; i++) 
+            {
+                Console.Write("Insira o nome de um aluno: ");
+                nome[i] = Console.ReadLine();
+                
+                Console.WriteLine("Insira a nota 1 deste aluno: ");
+                nota1 = Convert.ToDecimal(Console.ReadLine());
+
+                Console.WriteLine("Insira a nota 2 deste aluno: ");
+                nota2 = Convert.ToDecimal(Console.ReadLine());
+
+                Console.WriteLine("Insira a nota 3 deste aluno: ");
+                nota3 = Convert.ToDecimal(Console.ReadLine());
+
+                Console.WriteLine("Insira a nota 4 deste aluno: ");
+                nota4 = Convert.ToDecimal(Console.ReadLine());
+
+                media[i] = (nota1 + nota2 + nota3 + nota4) / 4;
+            }
+
+            for (int i = 0; i < nome.Length;i++)
+            {
+                Console.WriteLine($"A media do aluno {nome[i]} é {media[i]}");
+            }
+        }
+    }
+}
---
+++ NotasVetores/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NotasVetores
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+             Crie um programa que peça o nome e a idade de 10 pessoas.
+             Ao final o programa deve mostrar uma lista que indica se são maiores ou menores de idade.
+            */
+
+            string[] nomes = new string[2];
+            int[] idades = new int[2];
+
+            for (int i = 0; i < nomes.Length; i++)
+            {
+                Console.Write("Insira o nome de um aluno: ");
+                nomes[i] = Console.ReadLine();
+
+                Console.Write("Insira a idade do aluno: ");
+                idades[i] = Convert.ToInt32(Console.ReadLine());
+            }
+            
+            for (int i = 0;i < nomes.Length;i++)
+            {
+                if (idades[i] < 18)
+                {
+                    Console.WriteLine($"O aluno {nomes[i]} é de menor");
+                }
+                else
+                {
+                    Console.WriteLine($"O aluno {nomes[i]} é de maior");
+                }
+                
+            }
+
+        }
+    }
+}
